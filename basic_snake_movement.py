76fad2fcf58339334cc334f709662329fc158e8a
Arsh Sharma <sharmaa64@student.douglascollege.ca>
2021-11-03 19:17:29 +0530

    Added the food and increasing size of snake on eating it

--- basic_snake_movement.py
+++ basic_snake_movement.py
@@ -1,10 +1,11 @@
 # importing the graphics modules
 import turtle
+import random
 
-# define program constants
 WIDTH = 500
 HEIGHT = 500
 DELAY = 400  # in Milliseconds this controls the speed of the snake
+FOOD_SIZE = 10  # the size of the food
 
 # global variables
 
@@ -64,6 +65,24 @@ def get_distance(pos1, pos2):
     return distance
 
 
+# detect food collisions
+def food_collision():
+    global food_pos
+    if get_distance(snake[-1], food_pos) < 20:
+        food_pos = get_random_food_pos()
+        food.goto(food_pos)
+        return True
+
+    return False
+
+
+# to create the random food positions
+def get_random_food_pos():
+    x = random.randint(- WIDTH // 2 + FOOD_SIZE, WIDTH // 2 - FOOD_SIZE)
+    y = random.randint(- HEIGHT // 2 + FOOD_SIZE, HEIGHT // 2 - FOOD_SIZE)
+    return x, y
+
+
 # main method for the snake movement
 def move_snake():
     stamper.clearstamps()  # clears existing stamps
@@ -81,8 +100,10 @@ def move_snake():
         # add new head to snake body
         snake.append(new_head)
 
-        # remove the tail of the snake body
-        snake.pop(0)
+        # check for food collisions
+        if not food_collision():
+            # remove the tail of the snake body
+            snake.pop(0)  # keep the snake length same unless fed
 
         # Draw snake for the first time
         for segmentIntMvSnake in snake:
@@ -124,6 +145,15 @@ snake = [[0, 0], [20, 0], [40, 0], [60, 0]]
 for segment in snake:
     stamper.goto(segment[0], segment[1])
     stamper.stamp()
+
+# food
+food = turtle.Turtle()
+food.shape("circle")
+food.shapesize(FOOD_SIZE / 10)
+food.color("blue")
+food.penup()
+food_pos = get_random_food_pos()
+food.goto(food_pos)
 
 # Set animation in motion state
 move_snake()
